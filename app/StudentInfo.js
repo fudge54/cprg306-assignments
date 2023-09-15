8b9066fd5833c81be5312f2874af40f1bc7913ad
fudge54 <danman250000@gmail.com>
2023-09-15 14:07:37 -0600

    yeet

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Daniel Bourdage-Gorny</h1>
+            <div>CPRG 360 A</div>
+            <Link href="https://github.com/fudge54/cprg306-assignments">GitHub</Link>
+        </main>
+    );
+};
---
+++ app/week2/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import StudentInfo from '../StudentInfo';
+
+
+export default function week2(){
+    return (
+        <div>
+        <main>
+        <h3>My Shopping List</h3>
+
+        <StudentInfo/>
+        </main>
+        </div>
+    );
+};
