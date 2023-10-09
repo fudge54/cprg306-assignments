249e4b7c8fe818a7d060fcec9901fe828dfc36a9
fudge54 <danman250000@gmail.com>
2023-10-09 16:31:23 -0600

    yeet

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -22,30 +22,36 @@ export default function NewItem() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label className="name">Name</label>
-            <input type="text" id="name" name="name" value={name} onChange={(event) => setName(event.target.value)} required />
-            <label className="quantity">Quantity</label>
-            <input type="number" id="quantity" name="quantity" min="1" max="99" value={quantity} onChange={(event) => setQuantity(Number(event.target.value))} required />
-            <label className="category">Category</label>
-            <select id="category" name="category" value={category} onChange={(event) => setCategory(event.target.value)}>
-                <option value="produce">Produce</option>
-                <option value="dairy">Dairy</option>
-                <option value="bakery">Bakery</option>
-                <option value="meat">Meat</option>
-                <option value="frozen foods">Frozen Foods</option>
-                <option value="canned goods">Canned Goods</option>
-                <option value="dry goods">Dry Goods</option>
-                <option value="beverages">Beverages</option>
-                <option value="snacks">Snacks</option>
-                <option value="household">Household</option>
-                <option value="other">Other</option>
-            </select>
-            <button type="submit">Add Item</button>
-        </form>
-
-    )
-
-    
+        <div className="container mx-auto p-4">
+            <form onSubmit={handleSubmit} className="bg-white p-4 shadow-md rounded-lg">
+                <div className="mb-4">
+                    <label htmlFor="name" className="block text-gray-600 font-bold">Name</label>
+                    <input type="text" id="name" name="name" value={name} onChange={(event) => setName(event.target.value)} required className="w-full border rounded p-2" />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="quantity" className="block text-gray-600 font-bold">Quantity</label>
+                    <input type="number" id="quantity" name="quantity" min="1" max="99" value={quantity} onChange={(event) => setQuantity(Number(event.target.value))} required className="w-full border rounded p-2" />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="category" className="block text-gray-600 font-bold">Category</label>
+                    <select id="category" name="category" value={category} onChange={(event) => setCategory(event.target.value)} className="w-full border rounded p-2">
+                        <option value="produce">Produce</option>
+                        <option value="dairy">Dairy</option>
+                        <option value="bakery">Bakery</option>
+                        <option value="meat">Meat</option>
+                        <option value="frozen foods">Frozen Foods</option>
+                        <option value="canned goods">Canned Goods</option>
+                        <option value="dry goods">Dry Goods</option>
+                        <option value="beverages">Beverages</option>
+                        <option value="snacks">Snacks</option>
+                        <option value="household">Household</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div className="mb-4">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Item</button>
+                </div>
+            </form>
+        </div>
+    );
 }
-
